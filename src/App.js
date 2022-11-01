ad5491876f2f3e5adf9caed1871ee3abc58dc899
shaker <farooquishaker10@gmail.com>
2022-11-01 16:18:52 +0530

    Adding form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
+
 import Expenses from './components/Expenses/Expenses';
 
 
@@ -34,7 +36,7 @@ const App = () => {
 
     return (
        <div>
-        <h2>Let's get started</h2>
+        <NewExpense />
         <Expenses item={expenses}/>
      
         </div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,28 +6,14 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => { 
-
-    const [newTitle, setNewTitle] = useState("");
-
-   const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle(newTitle);
-    }
-
-    const changeHandler = (event) => {
-        setNewTitle(event.target.value);
-    }
-   
+       
     return (
         <Card className="expense-item">
           <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{ title }</h2>
+                <h2>{ props.title }</h2>
                 <div className="expense-item__price">${ props.amount }</div>
             </div>
-            <input type="text" value={newTitle} onChange={ changeHandler } />
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
 
     );
